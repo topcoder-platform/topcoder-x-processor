84ca9afa8c82beef0f111452e6a80e6f6a7efe5a
CWD <cwd@cloudspokes.com>
2017-12-13 13:28:42 -0500

    adding in event handler for offset resets. works now that we reset the offset appropriately

--- utils/kafka.js
+++ utils/kafka.js
@@ -17,13 +17,37 @@ const _ = require('lodash');
 const IssueService = require('../services/IssueService');
 const logger = require('./logger');
 
+const Offset = kafka.Offset;
+
 class Kafka {
   constructor() {
     this.client = new kafka.KafkaClient(config.KAFKA_OPTIONS);
-    this.consumer = new kafka.Consumer(this.client, [{topic: config.TOPIC, partition: 0}], {autoCommit: true});
+    this.consumer = new kafka.Consumer(this.client, [{topic: config.TOPIC, partition: config.PARTITION}], {autoCommit: true});
+    this.consumer.setOffset(config.TOPIC, 0, 0);
+    this.offset = new Offset(this.client);
+    logger.info(`connecting on topic: ${config.TOPIC}`);
   }
 
   run() {
+    this.consumer.on('error', (err) => {
+      logger.error(err);
+    });
+
+    this.consumer.on('offsetOutOfRange', (topic) => {
+      logger.debug(topic);
+      logger.info('offset OutOfRange. resetting.');
+      this.offset.fetch([topic], (errOffsetFetch, offsets) => {
+        if (errOffsetFetch) {
+          logger.error(errOffsetFetch);
+          return console.error(errOffsetFetch);
+        }
+
+        const min = Math.min(offsets[topic.topic][topic.partition]);
+        logger.info(`Setting offset to ${min}`);
+        return this.consumer.setOffset(config.TOPIC, topic.partition, min);
+      });
+    });
+
     this.consumer.on('message', (message) => {
       logger.info(`received message from kafka: ${message.value}`);
 
