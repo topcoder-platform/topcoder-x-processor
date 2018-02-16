37747d04c5e402584e84665072e3bf3f4bdc2067
CWD <cwd@cloudspokes.com>
2018-02-15 21:41:08 -0500

    fixing default in config for mapping url

--- config/default.js
+++ config/default.js
@@ -81,7 +81,7 @@ module.exports = {
   },
   EMAIL_SENDER_ADDRESS: process.env.EMAIL_SENDER_ADDRESS || '',
   ISSUE_BID_EMAIL_RECEIVER: process.env.ISSUE_BID_EMAIL_RECEIVER || '',
-  TC_RAGNAR_USER_MAPPING_URL: process.env.TC_RAGNAR_LOGIN_URL || 'http://localhost:3001/api/v1/admin/tcuser',
+  TC_RAGNAR_USER_MAPPING_URL: process.env.TC_RAGNAR_USER_MAPPING_URL || 'http://localhost:3001/api/v1/admin/tcuser',
   TC_RAGNAR_ADMIN_LOGIN_BODY: {
     username: "admin",
     password: "password"
