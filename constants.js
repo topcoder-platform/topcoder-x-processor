a46540e11c16eac904525f32cc64ff9b5d2b68dd
anonymousjaggu <jpsharma1993@gmail.com>
2020-11-03 16:57:42 +0545

    changes of challenge

--- constants.js
+++ constants.js
@@ -31,9 +31,22 @@ const CHALLENGE_STATUS = {
 
 const SERVICE_ERROR_STATUS = 500;
 
+// issue status
+const ISSUE_STATUS = {
+  CHALLENGE_CANCELLED: 'challenge_cancelled',
+  CHALLENGE_CREATION_PENDING: 'challenge_creation_pending',
+  CHALLENGE_CREATION_SUCCESSFUL: 'challenge_creation_successful',
+  CHALLENGE_CREATION_FAILED: 'challenge_creation_failed',
+  CHALLENGE_CREATION_RETRIED: 'challenge_creation_retried',
+  CHALLENGE_PAYMENT_SUCCESSFUL: 'challenge_payment_successful',
+  CHALLENGE_PAYMENT_PENDING: 'challenge_payment_pending',
+  CHALLENGE_PAYMENT_FAILED: 'challenge_payment_failed'
+};
+
 module.exports = {
   USER_ROLES,
   USER_TYPES,
   SERVICE_ERROR_STATUS,
-  CHALLENGE_STATUS
+  CHALLENGE_STATUS,
+  ISSUE_STATUS
 };
